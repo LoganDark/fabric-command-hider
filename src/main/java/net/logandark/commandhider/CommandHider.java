2200c074affbc9cc6b60b629fcdec659582e5ecb
LoganDark <logandarklock@gmail.com>
2022-03-17 10:50:09 -0700

    Permission inheritance thing

--- src/main/java/net/logandark/commandhider/CommandHider.java
+++ src/main/java/net/logandark/commandhider/CommandHider.java
@@ -1,6 +1,7 @@
 package net.logandark.commandhider;
 
 import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.command.CommandSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,17 @@ public class CommandHider {
 	public static final Logger LOGGER = LoggerFactory.getLogger(CommandHider.class);
 
 	public static <S extends CommandSource> boolean canUsePath(S source, String path, boolean def) {
-		return Permissions.check(source, "command." + path, def);
+		String permission = "commands." + path;
+
+		for (int i = permission.length(); i >= 0; i--) {
+			if (i == permission.length() || permission.charAt(i) == '.') {
+				permission = permission.substring(0, i);
+
+				TriState value = Permissions.getPermissionValue(source, permission);
+				if (value.getBoxed() != null) return value.get();
+			}
+		}
+
+		return def;
 	}
 }
